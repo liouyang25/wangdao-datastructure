221bc0b0fb5b732e7a5b02bb247c8e7b087166aa
liouyang25 <liouyang25@gmail.com>
2024-05-22 13:19:18 +0800

    finished SharedStack

--- chapter3/Stack/SharedStack/SharedStack.c
+++ chapter3/Stack/SharedStack/SharedStack.c
@@ -1,3 +1,80 @@
 //
 // Created by li on 2024/5/22.
 //
+#define MaxSize 20
+typedef int ElemType;
+typedef struct {
+    ElemType data[MaxSize];
+    int top0;   //0号栈的指针
+    int top1;   //1号栈的指针
+} SharedStack;
+
+//初始化栈
+int InitStack(SharedStack *sharedStack) {
+    sharedStack->top0 = -1; //初始化0号栈的指针为-1
+    sharedStack->top1 = MaxSize;    //初始化1号栈的指针为MaxSize
+    return 1;
+}
+
+//销毁栈
+int DestroyStack(SharedStack *sharedStack) {
+    sharedStack->top0 = -1; //初始化0号栈的指针为-1
+    sharedStack->top1 = MaxSize;    //初始化1号栈的指针为MaxSize
+    return 1;
+}
+
+//进栈
+int Push(SharedStack *sharedStack, ElemType elemType, int type) {
+    if (sharedStack->top0 + 1 == sharedStack->top1) return 0;
+    switch (type) {
+        case 0:
+            sharedStack->data[++sharedStack->top0] = elemType;
+            break;
+        case 1:
+            sharedStack->data[--sharedStack->top1] = elemType;
+            break;
+        default:
+            return 0;
+    }
+    return 1;
+}
+
+//出栈
+int Pop(SharedStack *sharedStack, ElemType *elemType, int type) {
+    switch (type) {
+        case 0:
+            if (sharedStack->top0 == -1)return 0;
+            *elemType = sharedStack->data[sharedStack->top0--];
+            break;
+        case 1:
+            if (sharedStack->top1 == MaxSize)return 0;
+            *elemType = sharedStack->data[sharedStack->top1++];
+            break;
+        default:
+            return 0;
+    }
+    return 1;
+}
+
+//获取栈顶元素
+int GetTop(SharedStack *sharedStack, ElemType *elemType, int type) {
+    switch (type) {
+        case 0:
+            if (sharedStack->top0 == -1)return 0;
+            *elemType = sharedStack->data[sharedStack->top0];
+            break;
+        case 1:
+            if (sharedStack->top1 == MaxSize)return 0;
+            *elemType = sharedStack->data[sharedStack->top1];
+            break;
+        default:
+            return 0;
+    }
+    return 1;
+}
+
+//判断栈是否为空
+int StackEmpty(SharedStack sharedStack) {
+    if (sharedStack.top0 == 0 && sharedStack.top1 == MaxSize)return 1;
+    return 0;
+}
--- chapter3/Stack/SharedStack/main.c
+++ chapter3/Stack/SharedStack/main.c
@@ -1,3 +1,19 @@
 //
 // Created by li on 2024/5/22.
 //
+#include <stdio.h>
+#include "SharedStack.c"
+
+int main() {
+    SharedStack sharedStack;
+    ElemType elemType;
+    InitStack(&sharedStack);
+    Push(&sharedStack, 1, 0);
+    Push(&sharedStack, 2, 1);
+    Pop(&sharedStack, &elemType, 0);
+    printf("0号栈出栈元素：%d\n", elemType);
+    Pop(&sharedStack, &elemType, 1);
+    printf("1号栈出栈元素：%d\n", elemType);
+    DestroyStack(&sharedStack);
+    return 0;
+}
